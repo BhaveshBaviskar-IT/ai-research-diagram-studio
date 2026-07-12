7cd79111e2deaf6a541cd76516f422413ca35a90
Bhavesh Baviskar <bhaveshbaviskar2007@gmail.com>
2026-07-12 19:02:32 +0530

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://ai-research-diagram-studio-1.onrender.com/generate"
+    baseURL: "https://ai-research-diagram-studio-1.onrender.com/"
 });
